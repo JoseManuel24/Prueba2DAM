708be8807d04c0ac81b8f313c691ef2751659d14
Fran Parra <franparra2@gmail.com>
2021-02-23 12:33:48 +0100

    Cuenta atras

--- PruebaGitHub/Form1.cs
+++ PruebaGitHub/Form1.cs
@@ -24,7 +24,10 @@ namespace PruebaGitHub
 
         private void button3_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Era un domingo en la tardeee y fui a los coches de choqueeeee, TIRORIRORIRORIIIII!!");
+            for (int i = 10; i != 0; i--){
+                MessageBox.Show("cuenta atras:"+i);
+            }
+            
         }
     }
 }
